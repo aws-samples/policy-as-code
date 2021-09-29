f29226ba3d806373e0e517604ec5bc1fc6dd5c8f
Josh Jaffe <josjaf@gmail.com>
2021-09-29 15:32:06 -0400

    pip requirements

--- cdk/app/app.py
+++ cdk/app/app.py
@@ -1,15 +1,5 @@
 from aws_cdk import (
-    aws_events as events,
-    aws_lambda as lambda_,
-    aws_lambda_destinations,
-    aws_sns,
-    aws_ssm,
-    aws_iam,
-    aws_s3,
-    aws_sns_subscriptions,
-    aws_events_targets as targets,
-    core,
-    aws_logs,
+    core
 )
 import os
 import pathlib
