aa11a45bf3517a70d4348e27aecd1b5e1cec92c7
Kenneth Jackson <jaknn@amazon.com>
2021-11-12 13:17:44 -0500

    Upgrading to CDK 2.0 syntax

--- cdk/app/app.py
+++ cdk/app/app.py
@@ -1,17 +1,18 @@
 from aws_cdk import (
-    core
+    App,
+    Environment,
 )
 import os
 import pathlib
 from s3_deployment import S3AppStack
 
-app = core.App()
+app = App()
 
 S3AppStack(app, "policy-as-code",
-    env=core.Environment(
-        account=os.environ["CDK_DEFAULT_ACCOUNT"],
-        region=os.environ["CDK_DEFAULT_REGION"]
-    ),
-    description='')
+           env=Environment(
+               account=os.environ["CDK_DEFAULT_ACCOUNT"],
+               region=os.environ["CDK_DEFAULT_REGION"]
+           ),
+           description='')
 
 app.synth()
